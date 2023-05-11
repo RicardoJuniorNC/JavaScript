870e52fcbb249e874b3784adf6a6b0721087e20f
RicardoJuniorNC <ricardo.junior1997@gmail.com>
2023-05-11 14:47:17 -0300

    funcionou

--- Aula12EX/EX014/script.js
+++ Aula12EX/EX014/script.js
@@ -1,24 +1,24 @@
-
 function carregar() {
-    console.log('function carregar() foi carregada');
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
+    var msg = document.querySelector('div#msg')
+    var foto = document.querySelector('img#imagem')
     var data = new Date()
-    var hora = data.getHours()
+    var hora = 19//data.getHours()
     
     msg.innerHTML = `Agora sao ${hora} horas.`
     
     if (hora >= 0 && hora < 12) {
         // BOM DIA
-    img.src = 'img/fotomanha.png'  
+    foto.src = "fotomanha.png";  
     document.body.style.background = '#dca401'
     } else if (hora >= 12 && hora <= 18) {
         // BOA TARDE
-    img.src = 'img/fototarde.png'
+    foto.src = "fototarde.png";
     document.body.style.background = '#ff4f00'
     } else {
         // BOA NOITE
-    img.src = 'img/fotonoite.png'
+    foto.src ="fotonoite.png";
     document.body.style.background = '#2d4776'
     }
 }
+
+document.onload = carregar()
